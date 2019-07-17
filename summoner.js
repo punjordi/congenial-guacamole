3f57147fecfc97be1dfc4998e7a98d70f6f6a419
punjordi <jordipun@yahoo.ca>
2019-07-17 14:48:10 -0600

    Started Script for Angular Module and Controller, Completed some CSS and html elements. Binded recentnames to List HTML dynamically

---
+++ summoner.js
@@ -0,0 +1,6 @@
+var myApp = angular.module("appModule",[]);
+
+myApp.controller("appController",function ($scope) {
+   $scope.recentNames = ["Placeholder sample recent name"];
+   $scope.newItem = "";
+});
