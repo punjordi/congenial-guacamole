3e5375e5e907b9393f183652cc8b298c4422eae7
punjordi <jordipun@yahoo.ca>
2019-07-17 22:20:42 -0600

    Bug Fixes and comments

--- summoner.js
+++ summoner.js
@@ -1,10 +1,18 @@
+//Initialize angular module
 var myApp = angular.module("appModule",[]);
+
+//Initialize angular controller that processes user input and displays a list of recent searches
+//that is stored on localstorage
 
 myApp.controller("appController",function ($scope) {
 
-
+    //Parse the localstorage string into the array
     $scope.recentNames = JSON.parse(localStorage.getItem("namesList"));
+    //Initialize variables
     $scope.newName = "";
+
+    //Function pushname that takes user input, checks for duplicates in the localstorage and if there is, pushes it to the front
+
     $scope.pushName = function () {
 
        if($scope.newName !== "") {
@@ -19,26 +27,20 @@ myApp.controller("appController",function ($scope) {
                $scope.recentNames.unshift($scope.newName);
                $scope.pushLocal($scope.recentNames);
                $scope.newName = "";
-
-
        }
-
-
     }
-
-
-
    };
+    //Function deletes the name in the list given an index.
    $scope.deleteName = function (index) {
        $scope.recentNames.splice(index,1);
+       $scope.pushLocal($scope.recentNames);
    };
+
+   //Helper function that pushes the array object into localstorage by first stringifying.
    $scope.pushLocal = function (arr) {
-
        localStorage.setItem("namesList",JSON.stringify(arr));
-
    }
 
 
-
-
 });
+
