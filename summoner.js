1b0155060dea7812411e440577e2bcd48edae015
punjordi <jordipun@yahoo.ca>
2019-07-17 21:25:44 -0600

    Finished persistence by pushing to localstorage. When user types an existing entry, checks duplicates and brings it to the front.

--- summoner.js
+++ summoner.js
@@ -1,6 +1,44 @@
 var myApp = angular.module("appModule",[]);
 
 myApp.controller("appController",function ($scope) {
-   $scope.recentNames = ["Placeholder sample recent name"];
-   $scope.newItem = "";
+
+
+    $scope.recentNames = JSON.parse(localStorage.getItem("namesList"));
+    $scope.newName = "";
+    $scope.pushName = function () {
+
+       if($scope.newName !== "") {
+
+           if($scope.recentNames.includes($scope.newName)){
+               $scope.deleteName($scope.recentNames.indexOf($scope.newName));
+               $scope.recentNames.unshift($scope.newName);
+               $scope.pushLocal($scope.recentNames);
+               $scope.newName = "";
+           }
+           else{
+               $scope.recentNames.unshift($scope.newName);
+               $scope.pushLocal($scope.recentNames);
+               $scope.newName = "";
+
+
+       }
+
+
+    }
+
+
+
+   };
+   $scope.deleteName = function (index) {
+       $scope.recentNames.splice(index,1);
+   };
+   $scope.pushLocal = function (arr) {
+
+       localStorage.setItem("namesList",JSON.stringify(arr));
+
+   }
+
+
+
+
 });
